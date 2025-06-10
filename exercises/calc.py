ad879ed3c43c31fec015986cceb27fbf9512d1d5
Mayank <mayank@webcraft.co.in>
2025-06-10 09:25:22 +0530

    Refactor calculator button commands to use a single click function for improved code efficiency

--- exercises/calc.py
+++ exercises/calc.py
@@ -12,46 +12,51 @@ e = Entry(window, width=56, borderwidth=5)
 e.place(x=0, y=0)
 
 # Buttons
-b = Button(window, text='1', width=12)
+def click(num):
+    result=e.get()
+    e.delete(0, END)
+    e.insert(0, str(result) + str(num))
+
+b = Button(window, text='1', width=12, command=lambda:click(1))
 b.place(x=10, y=60)
 
-b = Button(window, text='2', width=12)
+b = Button(window, text='2', width=12, command=lambda:click(2))
 b.place(x=100, y=60)
 
-b = Button(window, text='3', width=12)
+b = Button(window, text='3', width=12, command=lambda:click(3))
 b.place(x=190, y=60)
 
-b = Button(window, text='4', width=12)
+b = Button(window, text='4', width=12, command=lambda:click(4))
 b.place(x=10, y=120)
 
-b = Button(window, text='5', width=12)
+b = Button(window, text='5', width=12, command=lambda:click(5))
 b.place(x=100, y=120)
 
-b = Button(window, text='6', width=12)
+b = Button(window, text='6', width=12, command=lambda:click(6))
 b.place(x=190, y=120)
 
-b = Button(window, text='7', width=12)
+b = Button(window, text='7', width=12, command=lambda:click(7))
 b.place(x=10, y=180)
 
-b = Button(window, text='8', width=12)
+b = Button(window, text='8', width=12, command=lambda:click(8))
 b.place(x=100, y=180)
 
-b = Button(window, text='9', width=12)
+b = Button(window, text='9', width=12, command=lambda:click(9))
 b.place(x=190, y=180)
 
-b = Button(window, text='0', width=12)
+b = Button(window, text='0', width=12, command=lambda:click(0))
 b.place(x=10, y=240)
 
-b = Button(window, text='+', width=12)
+b = Button(window, text='+', width=12, command=lambda:click('+'))
 b.place(x=100, y=240)
 
-b = Button(window, text='-', width=12)
+b = Button(window, text='-', width=12, command=lambda:click('-'))
 b.place(x=190, y=240)
 
-b = Button(window, text='*', width=12)
+b = Button(window, text='*', width=12, command=lambda:click('*'))
 b.place(x=10, y=300)
 
-b = Button(window, text='/', width=12)
+b = Button(window, text='/', width=12, command=lambda:click('/'))
 b.place(x=100, y=300)
 
 b = Button(window, text='=', width=12)
